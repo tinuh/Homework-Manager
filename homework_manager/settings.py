422b1178e251e5d454190869f96a58ae28273179
tinuh <tinuvanapamula@gmail.com>
2020-03-20 13:21:50 -0400

    Preparing to Deploy to AWS

--- homework_manager/settings.py
+++ homework_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6$quaimi!xgfz#swhx5!&3q0v6r#x2hhk@uy5q(5f+*83)vqg0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'http://homework-manager-dev.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*', 'http://homework-manager-dev.us-east-1.elasticbeanstalk.com', '172.31.43.17', '172.31.80.154:140', '172.31.47.254']
 
 
 # Application definition
