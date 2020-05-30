ee0b8aabba474b9fa8b3cebf0b0a23cfca4183d4
tinuh <tinuvanapamula@gmail.com>
2020-05-30 14:22:55 -0400

    Migrate from sqlite over to postgresql for faster db.

--- homework_manager/settings.py
+++ homework_manager/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'homework_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASE = {
+#    'default':{
+#        "ENGINE": 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+#}
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hmanager',
+        "USER": 'postgres',
+        "PASSWORD": 'hmadmin',
+        "HOST": 'homework.tinu.tech',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
