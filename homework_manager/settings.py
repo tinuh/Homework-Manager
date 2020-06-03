e42b6862fb8800a50f83fd3f546176677e514280
tinu4 <tinuvanapamula@gmail.com>
2020-06-03 18:03:37 -0400

    minor change.

--- homework_manager/settings.py
+++ homework_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 if env("DEBUG"):
-    ALLOWED_HOSTS = ["localhost"]
+    ALLOWED_HOSTS = ["localhost", 'homework.tinu.tech']
 else:
     ALLOWED_HOSTS = ['homework.tinu.tech', "192.168.1.3"]
 
