3d4dde598a63a8a8b27a2af3ab43a5083a4a8fca
tinu4 <tinuvanapamula@gmail.com>
2020-06-05 23:54:10 -0400

    Minor Changes.

--- homework_manager/settings.py
+++ homework_manager/settings.py
@@ -98,9 +98,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DATABASE_NAME'),
-        "USER": env('DATABASE_USER'),
-        "PASSWORD": env('DATABASE_PWD'),
-        "HOST": env('DATABASE_HOST'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PWD'),
+        'HOST': env('DATABASE_HOST'),
         'PORT': '5432'
     }
 }
